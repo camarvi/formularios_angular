f864b65006df6666b8e97d88cd322ff5024c6e14
Camarvi <camarvi24@gmail.com>
2020-10-11 22:11:05 +0200

    mostrar datos en el form

---
+++ src/app/pages/template/template.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-template',
+  templateUrl: './template.component.html',
+  styleUrls: ['./template.component.css']
+})
+export class TemplateComponent implements OnInit {
+
+  usuario = {
+    nombre :'Carlos'
+  }
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  guardar( forma : NgForm){
+
+    console.log(forma);
+    console.log(forma.value);
+  }
+
+
+
+}
