47b177adf3ae986c2b61accfa08bd19285eabbcc
Camarvi <camarvi24@gmail.com>
2020-10-23 20:31:19 +0200

    Formularios Reactivos,  crear un formulario, FormGroup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 // MODULO PARA TRABAJAR CON FORMULARIOS
-import { FormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';  // Por template
+import { ReactiveFormsModule } from '@angular/forms'; //Formularios Reactivos
 
 //PETICIONES HTTP
 
@@ -23,6 +24,7 @@ import { ReactiveComponent } from './pages/reactive/reactive.component';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
+    ReactiveFormsModule,
     HttpClientModule
   ],
   providers: [],
--- src/app/pages/reactive/reactive.component.ts
+++ src/app/pages/reactive/reactive.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { FormGroup, FormBuilder } from '@angular/forms';
+
 @Component({
   selector: 'app-reactive',
   templateUrl: './reactive.component.html',
@@ -7,9 +9,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReactiveComponent implements OnInit {
 
-  constructor() { }
+  forma : FormGroup;
+
+  constructor(private fb : FormBuilder) { 
+
+    this.crearFormulario();
+
+  }
 
   ngOnInit() {
+  }
+
+  crearFormulario(){
+
+    this.forma = this.fb.group({
+      nombre : ['Kamarvi'],
+      apellidos : ['Gamer'],
+      correo : ['kamarvi_gamer@gmail.com'],
+    });
+
+  }
+
+
+  guardar(){
+    
+    console.log(this.forma);
+
   }
 
 }
