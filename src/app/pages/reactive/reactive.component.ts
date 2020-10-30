8bdf2fb1882c85c2c79cdc7aff8f87dc69a000ba
Camarvi <camarvi24@gmail.com>
2020-10-30 19:34:02 +0100

    validacion formularios reactivos

--- src/app/pages/reactive/reactive.component.ts
+++ src/app/pages/reactive/reactive.component.ts
@@ -20,10 +20,24 @@ export class ReactiveComponent implements OnInit {
   ngOnInit() {
   }
 
+  get nombreNoValido(){
+    return this.forma.get('nombre').invalid && this.forma.get('nombre').touched
+  }
+
+
+  get apellidoNoValido(){
+    return this.forma.get('apellidos').invalid && this.forma.get('apellidos').touched
+  }
+
+  get correoNoValido(){
+    return this.forma.get('correo').invalid && this.forma.get('correo').touched
+  }
+
+
   crearFormulario(){
 
     this.forma = this.fb.group({
-      nombre : ['', Validators.required],
+      nombre : ['', [Validators.required, Validators.minLength(5) ]],
       apellidos : ['', [Validators.required, Validators.minLength(5) ]],
       correo : ['', [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')]],
     });
@@ -34,6 +48,14 @@ export class ReactiveComponent implements OnInit {
   guardar(){
     
     console.log(this.forma);
+
+    if (this.forma.invalid) {
+      // recorrer los elementos del formulario para que se dispare las validaciones
+      return Object.values(this.forma.controls).forEach(control => {
+        control.markAsTouched(); //Lo pongo como pulsado
+      });
+
+    }
 
   }
 
