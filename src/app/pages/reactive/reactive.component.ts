8522a0ef9eb70737da00ac5b404e373e2ea6c172
Camarvi <camarvi24@gmail.com>
2020-11-15 21:55:53 +0100

    validar password iguales

--- src/app/pages/reactive/reactive.component.ts
+++ src/app/pages/reactive/reactive.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { FormGroup, FormBuilder, Validators, FormArray } from '@angular/forms';
+import { ValidadoresService } from '../../services/validadores.service';
 
 @Component({
   selector: 'app-reactive',
@@ -11,7 +12,9 @@ export class ReactiveComponent implements OnInit {
 
   forma : FormGroup;
 
-  constructor(private fb : FormBuilder) { 
+  constructor(private fb : FormBuilder,
+              private validadores : ValidadoresService) 
+    { 
 
     this.crearFormulario();
     //this.cargarDataAlFormulario();
@@ -46,18 +49,32 @@ export class ReactiveComponent implements OnInit {
     return this.forma.get('direccion.ciudad').invalid && this.forma.get('direccion.ciudad').touched
   }
 
+  get pass1NoValido(){
+    return this.forma.get('pass1').invalid && this.forma.get('pass1').touched
+  }
+
+
+  get pass2NoValido(){
+    const pass1 = this.forma.get('pass1').value
+    const pass2 = this.forma.get('pass2').value
+    return (pass1 === pass2) ? false : true
+  }
 
   crearFormulario(){
 
     this.forma = this.fb.group({
       nombre : ['', [Validators.required, Validators.minLength(5) ]],
-      apellidos : ['', [Validators.required, Validators.minLength(5) ]],
+      apellidos : ['', [Validators.required, Validators.minLength(5), this.validadores.noHerrera ]],
       correo : ['', [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')]],
+      pass1 : ['', Validators.required],
+      pass2 : ['', Validators.required], 
       direccion : this.fb.group({
         distrito : ['', Validators.required],
         ciudad : ['', Validators.required],
       }),
       pasatiempos : this.fb.array([]) 
+    },{
+      validators : [this.validadores.passwordsIguales('pass1','pass2')]
     });
 
   }
